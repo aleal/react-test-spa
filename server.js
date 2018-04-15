ff6eb2c4950afef83a043970b05990c11e9b2c43
aleal <aleal4343@gmail.com>
2018-04-14 23:11:58 -0300

    fixing static configs

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'public/index.html'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(port);
